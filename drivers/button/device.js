8c53dc06b90edfe90886f6b90139e3bd4b6b9da8
Olin <448222024@qq.com>
2023-12-29 11:02:07 +0800

    Add support for vibration
    Change-Id: I14d0f5d8f5837713c7de03bbe723eb4c34231d92

---
+++ drivers/button/device.js
@@ -0,0 +1,126 @@
+'use strict';
+
+const { ZigBeeDevice } = require("homey-zigbeedriver");
+const { CLUSTER } = require("zigbee-clusters");
+
+
+var flag;
+
+function startSleep(ms) {
+
+  return new Promise(resolve => {
+    flag = setTimeout(
+      () => resolve(), ms)
+  })
+}
+
+
+function cancelSleep() {
+  clearTimeout(flag);
+}
+
+
+class Button extends ZigBeeDevice {
+
+
+
+
+  /**
+   * onInit is called when the device is initialized.
+   */
+  async onNodeInit({ zclNode }) {
+    this.log('Button has been initialized');
+    // this.log(CLUSTER)
+
+    if (this.getClusterEndpoint(CLUSTER.MULTI_STATE_INPUT)) {
+      zclNode.endpoints[this.getClusterEndpoint(CLUSTER.MULTI_STATE_INPUT)].clusters[CLUSTER.MULTI_STATE_INPUT.NAME]
+        .on('attr.presentValue', this.onGetMultiStateInputPresentValueAttributeReport.bind(this));
+    }
+
+
+
+    if (this.getClusterEndpoint(CLUSTER.POWER_CONFIGURATION)) {
+      zclNode.endpoints[this.getClusterEndpoint(CLUSTER.POWER_CONFIGURATION)].clusters[CLUSTER.POWER_CONFIGURATION.NAME]
+        .on('attr.batteryPercentageRemaining', this.onBatteryPercentageRemainingAttributeReport.bind(this));
+    }
+  }
+
+
+  async displayButtonStatus(getPresentValue) {
+    // this.log("flag   ", flag)
+    if (getPresentValue == 0) {
+      this.setCapabilityValue("third_reality_button_capability", "Long press")
+    }
+    else if (getPresentValue == 1) {
+      this.setCapabilityValue("third_reality_button_capability", "Press")
+    }
+    else if (getPresentValue == 2) {
+      this.setCapabilityValue("third_reality_button_capability", "Double press")
+    }
+    else if (getPresentValue == 255) {
+      this.setCapabilityValue("third_reality_button_capability", "Release")
+    }
+
+    await startSleep(3000)
+  }
+
+  /**
+   * onAdded is called when the user adds the device, called just after pairing.
+   */
+  async onAdded() {
+    this.log('Button has been added');
+  }
+
+  async onGetMultiStateInputPresentValueAttributeReport(presentValue) {
+    var getPresentValue = presentValue
+    this.log("Button present Value is: ", String(presentValue))
+
+    if (flag) {
+      cancelSleep()
+    }
+
+    try {
+      await this.displayButtonStatus(getPresentValue)
+      this.setCapabilityValue("third_reality_button_capability", "")
+    } catch (error) {
+      this.log(error)
+    }
+  }
+
+  onBatteryPercentageRemainingAttributeReport(batteryPercentageRemaining) {
+    const batteryThreshold = this.getSetting('batteryThreshold') || 20;
+    this.log("measure_battery | powerConfiguration - batteryPercentageRemaining (%): ", batteryPercentageRemaining / 2);
+    this.setCapabilityValue('measure_battery', batteryPercentageRemaining / 2).catch(this.error);
+  }
+
+  /**
+   * onSettings is called when the user updates the device's settings.
+   * @param {object} event the onSettings event data
+   * @param {object} event.oldSettings The old settings object
+   * @param {object} event.newSettings The new settings object
+   * @param {string[]} event.changedKeys An array of keys changed since the previous version
+   * @returns {Promise<string|void>} return a custom message that will be displayed
+   */
+  async onSettings({ oldSettings, newSettings, changedKeys }) {
+    this.log('Button settings where changed');
+  }
+
+  /**
+   * onRenamed is called when the user updates the device's name.
+   * This method can be used this to synchronise the name to the device.
+   * @param {string} name The new name
+   */
+  async onRenamed(name) {
+    this.log('Button was renamed');
+  }
+
+  /**
+   * onDeleted is called when the user deleted the device.
+   */
+  async onDeleted() {
+    this.log('Button has been deleted');
+  }
+
+}
+
+module.exports = Button;
---
+++ drivers/vibration/device.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const { ZigBeeDevice } = require("homey-zigbeedriver");
+const { CLUSTER } = require("zigbee-clusters");
+
+class VibrationSensor extends ZigBeeDevice {
+
+  /**
+   * onInit is called when the device is initialized.
+   */
+  async onNodeInit({ zclNode }) {
+    this.log('Vibration Sensor has been initialized');
+
+
+    if (this.getClusterEndpoint(CLUSTER.IAS_ZONE)) {
+      zclNode.endpoints[this.getClusterEndpoint(CLUSTER.IAS_ZONE)].clusters[CLUSTER.IAS_ZONE.NAME].onZoneStatusChangeNotification = payload => {
+        this.onIASZoneStatusChangeNotification(payload)
+      };
+    }
+
+    if (this.getClusterEndpoint(CLUSTER.POWER_CONFIGURATION)) {
+      zclNode.endpoints[this.getClusterEndpoint(CLUSTER.POWER_CONFIGURATION)].clusters[CLUSTER.POWER_CONFIGURATION.NAME]
+        .on('attr.batteryPercentageRemaining', this.onBatteryPercentageRemainingAttributeReport.bind(this));
+    }
+  }
+
+
+  /**
+   * onAdded is called when the user adds the device, called just after pairing.
+   */
+  async onAdded() {
+    this.log('Vibration Sensor has been added');
+  }
+
+  onIASZoneStatusChangeNotification({ zoneStatus, extendedStatus, zoneId, delay, }) {
+    this.log('IASZoneStatusChangeNotification received:', zoneStatus, extendedStatus, zoneId, delay);
+    this.log('zoneStatus', zoneStatus.alarm1);
+    this.setCapabilityValue('alarm_generic', zoneStatus.alarm1).catch(this.error);
+  }
+
+  onBatteryPercentageRemainingAttributeReport(batteryPercentageRemaining) {
+    const batteryThreshold = this.getSetting('batteryThreshold') || 20;
+    this.log("measure_battery | powerConfiguration - batteryPercentageRemaining (%): ", batteryPercentageRemaining / 2);
+    this.setCapabilityValue('measure_battery', batteryPercentageRemaining / 2).catch(this.error);
+  }
+
+  /**
+   * onSettings is called when the user updates the device's settings.
+   * @param {object} event the onSettings event data
+   * @param {object} event.oldSettings The old settings object
+   * @param {object} event.newSettings The new settings object
+   * @param {string[]} event.changedKeys An array of keys changed since the previous version
+   * @returns {Promise<string|void>} return a custom message that will be displayed
+   */
+  async onSettings({ oldSettings, newSettings, changedKeys }) {
+    this.log('Vibration Sensor settings where changed');
+  }
+
+  /**
+   * onRenamed is called when the user updates the device's name.
+   * This method can be used this to synchronise the name to the device.
+   * @param {string} name The new name
+   */
+  async onRenamed(name) {
+    this.log('Vibration Sensor was renamed');
+  }
+
+  /**
+   * onDeleted is called when the user deleted the device.
+   */
+  async onDeleted() {
+    this.log('Vibration Sensor has been deleted');
+  }
+
+}
+
+module.exports = VibrationSensor;
